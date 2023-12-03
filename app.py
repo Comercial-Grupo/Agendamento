49aa8deee68ae738ae8a9d02cd489650d67abcc6
Carlos Tadeu L Baracho <122806666+Comercial-Grupo@users.noreply.github.com>
2023-12-03 15:51:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,53 +6,18 @@ import threading
 import requests
 import pytz
 from datetime import datetime
-
-# Inicialize o Deta com sua Project Key
-DETA_PROJECT_KEY = st.secrets["sua_project_key"]
-whatsapp_token = st.secrets["seu_token_whatsapp"]# Substitua com sua Project Key
-deta = Deta(DETA_PROJECT_KEY)
-db = deta.Base("tasks")
-
-fuso_horario_desejado = pytz.timezone("America/Sao_Paulo")
-
-def agendar_tarefa(horario, task_id):
-    try:
-        # Certifique-se de que 'horario' está no formato 'HH:MM'
-        horario_local = fuso_horario_desejado.localize(datetime.strptime(horario, "%H:%M"))
-        horario_utc = horario_local.astimezone(pytz.utc)
-
-        # Agende a tarefa usando o horário em UTC
-        schedule.every().day.at(horario_utc.strftime("%H:%M")).do(send_scheduled_message, task_id=task_id)
-    except ValueError as e:
-        print(f"Erro ao converter o horário: {e}")
-
-
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def enviar_email(outlook_email, senha, destinatario, assunto, corpo):
-    try:
-        msg = MIMEMultipart()
-        msg['From'] = outlook_email
-        msg['To'] = destinatario
-        msg['Subject'] = assunto
+# Inicialização e Configuração
+DETA_PROJECT_KEY = st.secrets["sua_project_key"]
+whatsapp_token = st.secrets["seu_token_whatsapp"]
+deta = Deta(DETA_PROJECT_KEY)
+db = deta.Base("tasks")
+fuso_horario_desejado = pytz.timezone("America/Sao_Paulo")
 
-        msg.attach(MIMEText(corpo, 'plain'))
-
-        server = smtplib.SMTP('smtp.office365.com', 587)
-        server.starttls()
-        server.login(outlook_email, senha)
-        text = msg.as_string()
-        server.sendmail(outlook_email, destinatario, text)
-        server.quit()
-        return "E-mail enviado com sucesso!"
-    except Exception as e:
-        return f"Erro ao enviar e-mail: {e}"
-    
-        
-
-# Função para enviar mensagens para o WhatsApp (mesma função anterior)
+# Função para enviar mensagens para o WhatsApp
 def send_link(recipient, token, message):
     url = f'https://server.api-wa.me/message/text?key={token}'
     headers = {
@@ -66,17 +31,29 @@ def send_link(recipient, token, message):
         return response.text
     else:
         return {"error": f"Request failed with status code {response.status_code}"}
-        # Faltava fechar o parêntese aqui
 
-
-    # ... (código da função)
+# Função para enviar e-mails
+def enviar_email(outlook_email, senha, destinatario, assunto, corpo):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = outlook_email
+        msg['To'] = destinatario
+        msg['Subject'] = assunto
+        msg.attach(MIMEText(corpo, 'plain'))
+        server = smtplib.SMTP('smtp.office365.com', 587)
+        server.starttls()
+        server.login(outlook_email, senha)
+        server.sendmail(outlook_email, destinatario, msg.as_string())
+        server.quit()
+        return "E-mail enviado com sucesso!"
+    except Exception as e:
+        return f"Erro ao enviar e-mail: {e}"
 
 # Função para agendar e enviar mensagens
 def send_scheduled_message(task_id):
     task = db.get(task_id)
     if task:
-        send_link(task["whatsapp"],whatsapp_token, f"Lembrete da Tarefa: {task['description']}")
-        # Adicione aqui a lógica para enviar e-mail
+        send_link(task["whatsapp"], whatsapp_token, f"Lembrete da Tarefa: {task['description']}")
 
 # Função para agendar tarefas
 def schedule_task():
@@ -87,23 +64,7 @@ def schedule_task():
 # Interface do Streamlit
 st.title("Agendador de Tarefas")
 
-with st.form("envio_imediato_form"):
-    st.write("Envio Imediato de Mensagem e E-mail")
-    destinatario_email = st.text_input("Destinatário do E-mail")
-    assunto_email = st.text_input("Assunto do E-mail")
-    corpo_email = st.text_area("Corpo do E-mail")
-    outlook_email = st.text_input("Seu E-mail do Outlook")
-    senha_email = st.text_input("Senha do E-mail", type="password")
-    botao_envio_imediato = st.form_submit_button("Enviar Agora")
-
-if botao_envio_imediato:
-    # Enviar WhatsApp
-    resposta_whatsapp = send_link(whatsapp, token, description)
-    st.write(resposta_whatsapp)
-
-    # Enviar E-mail
-    resposta_email = enviar_email(outlook_email, senha_email, destinatario_email, assunto_email, corpo_email)
-    st.write(resposta_email)
+# Formulário para agendar tarefas
 with st.form("task_form"):
     description = st.text_input("Descrição da Tarefa")
     task_time = st.time_input("Horário da Tarefa")
@@ -124,11 +85,24 @@ if submit_button:
     agendar_tarefa(task_data["time"], new_task["key"])
     print(f"Tarefa agendada para {task_data['time']}")
     print(f"Horário atual: {datetime.now(fuso_horario_desejado).strftime('%H:%M')}")
-
     threading.Thread(target=schedule_task, daemon=True).start()
     st.success("Tarefa agendada com sucesso!")
 
+# Formulário para envio imediato
+with st.form("envio_imediato_form"):
+    st.write("Envio Imediato de Mensagem e E-mail")
+    destinatario_email = st.text_input("Destinatário do E-mail")
+    assunto_email = st.text_input("Assunto do E-mail")
+    corpo_email = st.text_area("Corpo do E-mail")
+    outlook_email = st.text_input("Seu E-mail do Outlook")
+    senha_email = st.text_input("Senha do E-mail", type="password")
+    botao_envio_imediato = st.form_submit_button("Enviar Agora")
 
+if botao_envio_imediato:
+    resposta_whatsapp = send_link(whatsapp, token, description)
+    st.write(resposta_whatsapp)
+    resposta_email = enviar_email(outlook_email, senha_email, destinatario_email, assunto_email, corpo_email)
+    st.write(resposta_email)
 
 # Expander para mostrar tarefas agendadas
 with st.expander("Ver Tarefas Agendadas"):
@@ -136,6 +110,11 @@ with st.expander("Ver Tarefas Agendadas"):
     for task in tasks:
         st.write(f"{task['description']} - Horário: {task['time']}")
 
-
-
-#Aqui você pode adicionar mais lógica para marcar tarefas como concluídas
+# Função para agendar tarefa
+def agendar_tarefa(horario, task_id):
+    try:
+        horario_local = fuso_horario_desejado.localize(datetime.strptime(horario, "%H:%M"))
+        horario_utc = horario_local.astimezone(pytz.utc)
+        schedule.every().day.at(horario_utc.strftime("%H:%M")).do(send_scheduled_message, task_id=task_id)
+    except ValueError as e:
+        print(f"Erro ao converter o horário: {e}")
