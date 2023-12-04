d34560fb6f86eb858b12a57315f861a5206f76b4
Carlos Tadeu L Baracho <122806666+Comercial-Grupo@users.noreply.github.com>
2023-12-03 21:37:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,91 +1,90 @@
+import streamlit as st
 from deta import Deta
 import requests
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import pytz
 from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-import time
 
 # Configuração
-DETA_PROJECT_KEY = "sua_project_key"
-whatsapp_token = "seu_token_whatsapp"
+DETA_PROJECT_KEY = st.secrets["sua_project_key"]
+whatsapp_token = st.secrets["seu_token_whatsapp"]
 deta = Deta(DETA_PROJECT_KEY)
 db = deta.Base("tasks")
 fuso_horario_desejado = pytz.timezone("America/Sao_Paulo")
 
-# Inicialize o scheduler
-scheduler = BackgroundScheduler()
-scheduler.start()
+# Função para enviar mensagens para o WhatsApp
+def send_link(recipient, token, message):
+    url = f'https://server.api-wa.me/message/text?key={token}'
+    headers = {
+        'accept': '*/*',
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
 
-# Funções send_link e enviar_email permanecem as mesmas
+    payload = {
+        "messageData": {
+            "to": recipient,
+            "text": message
+        }
+    }
 
-# Função para agendar e enviar mensagens
-def send_scheduled_message(task_id):
-    task = db.get(task_id)
-    if task:
-        send_link(task["whatsapp"], task["token"], f"Lembrete da Tarefa: {task['description']}")
-        # Lógica para enviar e-mail
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return {"error": f"Request failed with status code {response.status_code}"}
 
-def agendar_tarefas():
+# Função para enviar e-mails
+def enviar_email(outlook_email, senha, destinatario, assunto, corpo):
     try:
-        tasks = db.fetch().items
-        for task in tasks:
-            horario = task['time']
-            task_id = task['key']
-            horario_local = fuso_horario_desejado.localize(datetime.strptime(horario, "%H:%M"))
-            horario_utc = horario_local.astimezone(pytz.utc)
-            scheduler.add_job(send_scheduled_message, 'cron', day_of_week='mon-sun', hour=horario_utc.hour, minute=horario_utc.minute, args=[task_id])
+        msg = MIMEMultipart()
+        msg['From'] = outlook_email
+        msg['To'] = destinatario
+        msg['Subject'] = assunto
+        msg.attach(MIMEText(corpo, 'plain'))
+        server = smtplib.SMTP('smtp.office365.com', 587)
+        server.starttls()
+        server.login(outlook_email, senha)
+        server.sendmail(outlook_email, destinatario, msg.as_string())
+        server.quit()
+        return "E-mail enviado com sucesso!"
     except Exception as e:
-        print(f"Erro ao buscar tarefas: {e}")
-
-agendar_tarefas()
-# Mantém o script rodando
-while True:
-    time.sleep(60)
-
+        return f"Erro ao enviar e-mail: {e}"
 
 # Interface do Streamlit
-st.title("Agendador de Tarefas")
+st.title("Disparador de Mensagens")
 
-# Formulário para agendar tarefas
-with st.form("task_form"):
-    description = st.text_input("Descrição da Tarefa")
-    task_time = st.time_input("Horário da Tarefa", step=300)
-    email = st.text_input("E-mail")
-    whatsapp = st.text_input("Número do WhatsApp")
-    token = st.text_input("Token do WhatsApp", type="password")
-    submit_button = st.form_submit_button("Agendar Tarefa")
-
-if submit_button:
-    task_data = {
-        "description": description,
-        "time": str(task_time),
-        "email": email,
-        "whatsapp": whatsapp,
-        "token": token
-    }
-    new_task = db.put(task_data)
-    st.success("Tarefa agendada com sucesso!")
+# Sidebar com modelos prontos
+modelos = {"Modelo 1": "Texto do Modelo 1", "Modelo 2": "Texto do Modelo 2"}
+modelo_selecionado = st.sidebar.selectbox("Escolha um modelo de mensagem", list(modelos.keys()))
+mensagem_modelo = modelos[modelo_selecionado]
+st.sidebar.text_area("Texto do modelo:", mensagem_modelo, disabled=True)
 
 # Formulário para envio imediato
 with st.form("envio_imediato_form"):
     st.write("Envio Imediato de Mensagem e E-mail")
-    destinatario_email = st.text_input("Destinatário do E-mail")
-    assunto_email = st.text_input("Assunto do E-mail")
-    corpo_email = st.text_area("Corpo do E-mail")
+    destinatarios_email = st.text_area("Destinatários do E-mail (separados por vírgula)")
+    destinatarios_whatsapp = st.text_area("Números do WhatsApp (separados por vírgula)")
+    assunto_email = st.text_input("Assunto do E-mail", value=modelo_selecionado)
+    corpo_email = st.text_area("Corpo do E-mail", value=mensagem_modelo)
     outlook_email = st.text_input("Seu E-mail do Outlook")
     senha_email = st.text_input("Senha do E-mail", type="password")
+    opcao_envio = st.selectbox("Enviar por", ["Ambos", "Apenas E-mail", "Apenas WhatsApp"])
     botao_envio_imediato = st.form_submit_button("Enviar Agora")
 
 if botao_envio_imediato:
-    resposta_whatsapp = send_link(whatsapp, token, description)
-    st.write(resposta_whatsapp)
-    resposta_email = enviar_email(outlook_email, senha_email, destinatario_email, assunto_email, corpo_email)
-    st.write(resposta_email)
+    destinatarios_email_lista = destinatarios_email.split(',')
+    destinatarios_whatsapp_lista = destinatarios_whatsapp.split(',')
+    
+    if opcao_envio in ["Ambos", "Apenas E-mail"]:
+        for email in destinatarios_email_lista:
+            resposta_email = enviar_email(outlook_email, senha_email, email.strip(), assunto_email, corpo_email)
+            st.write(f"Resposta E-mail para {email}: {resposta_email}")
 
-
-with st.expander("Ver Tarefas Agendadas"):
-    tasks = db.fetch().items
-    for task in tasks:
-        st.write(f"{task['description']} - Horário: {task['time']}")
-import atexit
-atexit.register(lambda: scheduler.shutdown())
+    if opcao_envio in ["Ambos", "Apenas WhatsApp"]:
+        for whatsapp in destinatarios_whatsapp_lista:
+            resposta_whatsapp = send_link(whatsapp.strip(), whatsapp_token, mensagem_modelo)
+            st.write(f"Resposta WhatsApp para {whatsapp}: {resposta_whatsapp}")
